7b4a3df7fcbc72408e91765c081a2ba34058aaf7
Fabien Dennel <fdennel@MacBook-Air-de-Fabien.local>
2024-08-01 09:30:44 +0200

    v1

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".fa").click(function(){
+        $(".responsive_links").fadeToggle(500);
+    });
+
+    $(".responsive_links li").click(function(){
+        $(".responsive_links").fadeToggle(500);
+    });
+});
